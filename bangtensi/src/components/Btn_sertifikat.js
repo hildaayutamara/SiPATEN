0d1615dcabc451f9b5748383fdb478d23b0041e1
hildaayutamara <hildaayutamara52@gmail.com>
2022-05-17 22:24:27 +0700

    pdf viewer

--- bangtensi/src/components/Btn_sertifikat.js
+++ bangtensi/src/components/Btn_sertifikat.js
@@ -2,8 +2,17 @@ import React, { useState, useEffect } from "react";
 import { Button, Modal, Table } from "react-bootstrap";
 import "./App.css";
 import { BsPlusLg } from "react-icons/bs";
+import { FaEye } from "react-icons/fa";
 import AddFormSertifikat from "./AddFormSertifikat";
 import axios from "axios";
+
+import { Viewer, Worker } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+
+const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
 const Btn_sertifikat = () => {
 
@@ -23,6 +32,19 @@ const Btn_sertifikat = () => {
       getdata();
     }, []);
     console.log("database>>>>>>>", database);
+
+    const handleLihat=() => {
+    <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.12.313/build/pdf.worker.js">
+        <div style={{ height: '750px' }}>
+            <Viewer
+                fileUrl={`${process.env.PUBLIC_URL}/pdf-open-parameters.pdf`}
+                plugins={[
+                    defaultLayoutPluginInstance,
+                ]}
+            />
+        </div>
+    </Worker>
+    }
 
   return (
     <>
@@ -50,7 +72,12 @@ const Btn_sertifikat = () => {
               <td>{item?.nomor}</td>
               <td>{item?.nama}</td>
               <td>{item?.update}</td>
-              <td>{item?.lihat}</td>
+              <td>
+                    <Button variant="success">
+                      <span className="me-2">Lihat Dokumen</span>
+                      <FaEye />
+                    </Button>
+                </td>
             </tr>
           ))}
         </tbody>
